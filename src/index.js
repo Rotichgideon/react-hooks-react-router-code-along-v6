94443648821419e71a20abd8bba621f98be30d86
gideon rotich <gideonrotich2001@gmail.com>
2024-05-09 11:58:36 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
